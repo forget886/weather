09ab7a98eda45e98ad21067914840aa560edcd54
zhanghui <zhanghui@souche.com>
2017-03-30 10:58:00 +0800

    util

---
+++ souche-chengxu/src/main/java/com/wuxi/util/ExecutePoolUtil.java
@@ -0,0 +1,32 @@
+package com.wuxi.util;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutePoolUtil {
+
+	private ExecutorService executorService = null;
+	private int corePoolSize = 4;
+	private int maximumPoolSize = 10;
+	/**
+	 * 线程池维护线程所允许的空闲时间
+	 */
+	private long keepAliveTime = 5;
+	
+	public void init(){
+		executorService = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(10), new RejectedExecutionHandler() {
+			@Override
+			public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+				try {
+					executor.getQueue().put(r);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
