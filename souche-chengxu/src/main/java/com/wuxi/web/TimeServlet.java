01dc29aa4d80616590aba2bc2f37b495b3874397
zhanghui <zhanghui@souche.com>
2017-06-13 11:00:09 +0800

    realpath

--- souche-chengxu/src/main/java/com/wuxi/web/TimeServlet.java
+++ souche-chengxu/src/main/java/com/wuxi/web/TimeServlet.java
@@ -31,6 +31,7 @@ public class TimeServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 				throws ServletException, IOException{
+		System.out.println(getServletContext().getRealPath("/"));
 		System.out.println("认证"+req.isUserInRole("admin"));
 		 System.out.println("url: " + req.getRequestURL());
 		 System.out.println("host: " + req.getHeader("host") + req.getRemoteHost());
