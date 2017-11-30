b81638b204a6ac83313444b0621f632a47a255ec
zhanghui <zhanghui@souche.com>
2017-11-30 14:27:52 +0800

    util

---
+++ souche-chengxu/src/main/java/com/wuxi/util/MD5Utils.java
@@ -0,0 +1,31 @@
+package com.wuxi.util;
+
+import java.security.MessageDigest;
+
+public class MD5Utils {
+
+    public static String md5(String input) {
+        StringBuffer sb = new StringBuffer();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(input.getBytes());
+            byte b[] = md.digest();
+            int i;
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    sb.append("0");
+                sb.append(Integer.toHexString(i));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return sb.toString();
+   }
+    
+   public static void main(String[] args){
+       
+   }
+}
